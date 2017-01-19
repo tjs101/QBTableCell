ef9976a659958bece43cb1a3b4c749d6cfe3a0d8
quentin <quentin@chenchengs-Mac-mini.local>
2017-01-19 14:32:21 +0800

    新增

---
+++ QBTableCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QBTableCell
+//
+//  Created by quentin on 2017/1/19.
+//  Copyright © 2017年 Quentin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ QBTableCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QBTableCell
+//
+//  Created by quentin on 2017/1/19.
+//  Copyright © 2017年 Quentin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
